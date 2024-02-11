70a1ca7857c3153d7cbf78a03a8fc4d7acc88b8a
Nazbro <122434356+Najmuzahir1147@users.noreply.github.com>
2024-02-11 17:42:46 +0700

    Add files via upload

--- homepage.js
+++ homepage.js
@@ -1,3 +1,4 @@
+//StickyNavbar
 window.onscroll = function () {
   myFunction();
 };
@@ -10,3 +11,19 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+//TransparentNavbar
+document.addEventListener("DOMContentLoaded", function() {
+  window.addEventListener("scroll", function() {
+    var navbar = document.getElementById("navbod");
+    var text = document.querySelectorAll(".mycontent");
+    text.forEach(function(text) {
+      
+    });
+    if (window.scrollY > 0) {
+      navbar.style.backgroundColor = "white";
+      text.style.color = "white";
+    } else {
+      navbar.style.backgroundColor = "transparent";
+    }
+  });
+});
